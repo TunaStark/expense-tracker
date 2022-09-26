597bf896c63e326f20d4557ca29a1c4a37d70b00
Tuna Güralp <57902543+TunaStark@users.noreply.github.com>
2022-09-26 22:28:59 +0300

    Add ant design page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
+import { Routes, Route } from 'react-router-dom'
+import SignUp from "./components/SignUp";
+import { Breadcrumb, Layout, Menu } from 'antd';
+
+
+
 function App() {
+  const { Header, Content, Footer } = Layout;
+
   return (
-    <>
-      Expense Tracker
-    </>
+    <Layout>
+      <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={new Array(3).fill(null).map((_, index) => ({
+            key: String(index + 1),
+            label: `nav ${index + 1}`,
+          }))}
+        />
+      </Header>
+      <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <Routes>
+
+          <Route path="/register" element={<SignUp />} />
+        </Routes>
+
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
   );
 }
 
