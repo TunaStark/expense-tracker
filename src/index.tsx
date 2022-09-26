36b46a9eaf9a053c3b34acf1ce0e291816f3a95b
Tuna Güralp <57902543+TunaStark@users.noreply.github.com>
2022-09-26 22:29:44 +0300

    Create store in instance

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,21 @@
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from "redux-thunk";
 import 'antd/dist/antd.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from './App';
+import rootReducer from './store';
+
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
+    document.getElementById("root")
+);
