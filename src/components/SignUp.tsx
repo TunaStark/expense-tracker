3ea8459b143af2b515b922e904e32fd60dc863c8
Tuna Güralp <57902543+TunaStark@users.noreply.github.com>
2022-10-02 17:53:53 +0300

    Add password mask and edit form

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -25,20 +25,23 @@ const SignUp = () => {
     return (
         <>
             <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
-                <Form.Item name={['user', 'name']} label="Name" rules={[{ required: true }]}>
+                <Form.Item name="username" label="Username" rules={[{ required: true }]}>
                     <Input />
                 </Form.Item>
-                <Form.Item name={['user', 'email']} label="Email" rules={[{ type: 'email' }]}>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[
+                        { required: true, message: 'Please input your password!', min: 6 }
+                    ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item name="email" label="Email" rules={[{ type: 'email', required: true }]}>
                     <Input />
                 </Form.Item>
-                <Form.Item name={['user', 'age']} label="Age" rules={[{ type: 'number', min: 0, max: 99 }]}>
-                    <InputNumber />
-                </Form.Item>
-                <Form.Item name={['user', 'website']} label="Website">
+                <Form.Item name="full_name" label="Full Name">
                     <Input />
-                </Form.Item>
-                <Form.Item name={['user', 'introduction']} label="Introduction">
-                    <Input.TextArea />
                 </Form.Item>
                 <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
                     <Button type="primary" htmlType="submit">
